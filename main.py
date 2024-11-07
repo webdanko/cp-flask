a48cfe8496a8d146e7a900fe45a1a4fed975f93a
Eli Abrosimov <eli.abrosimiov@gmail.com>
2024-11-07 16:19:01 +0200

    add: entity list route test

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/", response_class=PlainTextResponse)
 async def root():
     return "Timeweb Cloud + FastAPI = ❤️"
+
+@app.get("/api/{entity_type}/")
+def get_entity_list(entity_type: str):
+    return_list = []
+    return_list.append({
+        'type' : entity_type,
+        'id' : 1,
+        'name' : 'name_1'
+    })
+    return return_list
