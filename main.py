123ceb81abf4c28fbfbad45f340861de0dea8320
Eli Abrosimov <eli.abrosimiov@gmail.com>
2024-11-07 19:55:19 +0200

    update: infra search around

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import overpass
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
-
-def get_overpass_around(lat, lon, radius):
+def get_overpass_api():
     api = overpass.API(endpoint="https://maps.mail.ru/osm/tools/overpass/api/interpreter")
-    request = "nwr(around:%s, %s, %s)" % (radius, lat, lon)
-    result = api.get(request)
+    return api
+
+def prepare_filter(key, type="nwr", area="area"):
+    result = "%s[%s](%s);" % (type, key, area)
+    return result
+
+def prepare_around(lat, lon, radius):
+    result = "around:%s, %s, %s" % (radius, lat, lon)
+    return result
+
+def prepare_overpass_infrastructure(around, array):
+    filter = ''
+    type = 'nwr'
+    for k in array:
+        filter += prepare_filter(k,type,around)
+                 
+    result = "(%s);" % (filter)
     return result
 
 app = FastAPI()
@@ -17,5 +31,18 @@ async def root():
 
 @app.get("/api/around/{lat}/{lon}/{radius}")
 def get_entity_list(lat: float, lon: float, radius: float):
-    result = get_overpass_around(lat, lon, radius)
+    around = prepare_around(lat, lon, radius)
+    array = [
+        "amenity=school",
+        "amenity=college",
+        "amenity=music_school",
+        "amenity=university",
+        "amenity=hospital",
+        "amenity=clinic",
+        "amenity=bus_station",
+        "highway=bus_stop",
+    ]
+    request = prepare_overpass_infrastructure(around, array)
+    api = get_overpass_api()
+    result = api.get(request)
     return result
